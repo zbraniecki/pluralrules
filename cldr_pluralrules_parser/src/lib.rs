0c9e7caf9ee0ef36a3210e3da26cae76542763be
unclenachoduh <kekoariggin@gmail.com>
2018-07-09 10:31:17 -0700

    Crate descrip

--- cldr_pluralrules_parser/src/lib.rs
+++ cldr_pluralrules_parser/src/lib.rs
@@ -1,3 +1,29 @@
+//! Given a string reference of a plural rule, will return the AST representation of that rule.
+//!
+//! # Examples
+//!
+//! ```
+//! let condition = Condition(vec![
+//!     AndCondition(vec![Relation {
+//!         expression: Expression {
+//!             operand: Operand('i'),
+//!             modulus: None,
+//!         },
+//!         operator: Operator::Is,
+//!         range_list: RangeList(vec![RangeListItem::Value(Value(5))]),
+//!     }]),
+//!     AndCondition(vec![Relation {
+//!         expression: Expression {
+//!             operand: Operand('v'),
+//!             modulus: None,
+//!         },
+//!         operator: Operator::Within,
+//!         range_list: RangeList(vec![RangeListItem::Value(Value(2))]),
+//!     }]),
+//! ])
+//!
+//! assert_eq!(condition, parse_plural_rule("i is 5 or v within 2"))
+//! ```
 #[macro_use]
 extern crate nom;
 
