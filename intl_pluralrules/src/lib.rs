95e238f5b80a874ca9b6e8f8020aaf1777da583f
Zibi Braniecki <gandalf@mozilla.com>
2018-07-11 16:16:33 -0700

    Add select_plural_category function

--- intl_pluralrules/src/lib.rs
+++ intl_pluralrules/src/lib.rs
@@ -1,1 +1,29 @@
 pub mod operands;
+mod rules;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum PluralCategory {
+    ONE,
+    TWO,
+    FEW,
+    MANY,
+    OTHER
+}
+
+pub fn select_plural_category<S: ToString>(lang: &str, number: S) -> PluralCategory {
+    let f = rules::get_pr(lang);
+    let ops = operands::PluralOperands::new(number);
+    f(ops)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::select_plural_category;
+    use super::PluralCategory;
+    #[test]
+    fn it_works() {
+        assert_eq!(select_plural_category("naq", 1), PluralCategory::ONE);
+        assert_eq!(select_plural_category("naq", 2), PluralCategory::TWO);
+        assert_eq!(select_plural_category("naq", 5), PluralCategory::OTHER);
+    }
+}
