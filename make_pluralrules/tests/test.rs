d2529193836d97f305b5fde336e27c1ab6fd1d9b
unclenachoduh <kekoariggin@gmail.com>
2018-08-02 09:51:54 -0700

    make_pr: within test

--- make_pluralrules/tests/test.rs
+++ make_pluralrules/tests/test.rs
@@ -14,7 +14,7 @@ fn read_file(path: &str) -> Result<String, io::Error> {
 }
 
 #[test]
-fn simple_test() {
+fn full_cldr_test() {
     let cardinal_json = read_file("./tests/fixtures/cldr_pluralrules_cardinals_33.json")
         .expect("Could not read input json");
     let ordinal_json = read_file("./tests/fixtures/cldr_pluralrules_ordinals_33.json")
@@ -23,6 +23,18 @@ fn simple_test() {
         read_file("./tests/fixtures/cldr_pluralrules_33.rs").expect("Could not read output rs");
 
     let output = generate_rs(&[cardinal_json, ordinal_json]);
+
+    assert_eq!(output_rs, output);
+}
+
+#[test]
+fn within_test() {
+    let cardinal_json = read_file("./tests/fixtures/cldr_pluralrules_cardinals_test.json")
+        .expect("Could not read input json");
+    let output_rs =
+        read_file("./tests/fixtures/cldr_pluralrules_test.rs").expect("Could not read output rs");
+
+    let output = generate_rs(&[cardinal_json]);
 
     assert_eq!(output_rs, output);
 }
