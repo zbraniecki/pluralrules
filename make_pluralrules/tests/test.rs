75f1d88d5d587dbd088963e4efbafcfa5bd4a81b
unclenachoduh <kekoariggin@gmail.com>
2018-08-02 18:42:33 -0700

    make_pr: requested changes to tests

--- make_pluralrules/tests/test.rs
+++ make_pluralrules/tests/test.rs
@@ -30,23 +30,23 @@ fn full_cldr_test() {
 #[test]
 fn within_test() {
     let text = String::from(
-"{
-  \"supplemental\": {
-    \"version\": {
-      \"_number\": \"$Revision: 13898 $\",
-      \"_unicodeVersion\": \"10.0.0\",
-      \"_cldrVersion\": \"0\"
+r#"{
+  "supplemental": {
+    "version": {
+      "_number": "$Revision: 13898 $",
+      "_unicodeVersion": "10.0.0",
+      "_cldrVersion": "0"
     },
-    \"plurals-type-cardinal\": {
-      \"test\": {
-        \"pluralRule-count-one\": \"n = 1 @integer 1 @decimal 1.0, 1.00, 1.000, 1.0000\",
-        \"pluralRule-count-two\": \"n % 10 within 1..2 @ 11, 12, 21, 22...\",
-        \"pluralRule-count-few\": \"n within 2..10 and i not within 8..9 @ 1.1, 1.2, 2, 3, 4, 5, 6, 7, 10\",
-        \"pluralRule-count-other\": \" @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0~0.9, 1.1~1.6, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …\"
+    "plurals-type-cardinal": {
+      "test": {
+        "pluralRule-count-one": "n = 1 @integer 1 @decimal 1.0, 1.00, 1.000, 1.0000",
+        "pluralRule-count-two": "n % 10 within 1..2 @ 11, 12, 21, 22...",
+        "pluralRule-count-few": "n within 2..10 and i not within 8..9 @ 1.1, 1.2, 2, 3, 4, 5, 6, 7, 10",
+        "pluralRule-count-other": " @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0~0.9, 1.1~1.6, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …"
       }
     }
   }
-}");
+}"#);
     let output_rs = read_file("./tests/fixtures/cldr_pluralrules_within_test.rs")
         .expect("Could not read output rs");
 
@@ -59,21 +59,21 @@ fn within_test() {
 #[should_panic]
 fn bad_type_test() {
     let text = String::from(
-"{
-  \"supplemental\": {
-    \"version\": {
-      \"_number\": \"$Revision: 13898 $\",
-      \"_unicodeVersion\": \"10.0.0\",
-      \"_cldrVersion\": \"0\"
+r#"{
+  "supplemental": {
+    "version": {
+      "_number": "$Revision: 13898 $",
+      "_unicodeVersion": "10.0.0",
+      "_cldrVersion": "0"
     },
-    \"plurals-type-cardinals\": {
-      \"test\": {
-        \"pluralRule-count-one\": \"n = 1 @integer 1 @decimal 1.0, 1.00, 1.000, 1.0000\",
-        \"pluralRule-count-other\": \" @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0~0.9, 1.1~1.6, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …\"
+    "plurals-type-cardinals": {
+      "test": {
+        "pluralRule-count-one": "n = 1 @integer 1 @decimal 1.0, 1.00, 1.000, 1.0000",
+        "pluralRule-count-other": " @integer 0, 2~16, 100, 1000, 10000, 100000, 1000000, … @decimal 0.0~0.9, 1.1~1.6, 10.0, 100.0, 1000.0, 10000.0, 100000.0, 1000000.0, …"
       }
     }
   }
-}");
+}"#);
 
     generate_rs(&[text]);
 }
